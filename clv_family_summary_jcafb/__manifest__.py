b286fd2db3391771c077eca5f675a6536404182d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-12-17 16:02:23 -0300

    [IMP] clv_family_summary_jcafb (ADD security/ir.model.access.csv)

--- clv_family_summary_jcafb/__manifest__.py
+++ clv_family_summary_jcafb/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Family Summary (for CLVhealth-JCAFB Solution)',
     'summary': 'Family Summary Module used in CLVhealth-JCAFB Solution.',
-    'version': '12.0.4.0',
+    'version': '14.0.5.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
     'category': 'CLVsol Solutions',
     'license': 'AGPL-3',
@@ -15,6 +15,7 @@
         'clv_summary',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'data/family_summary.xml',
         'views/summary_view.xml',
         'wizard/family_summary_setup_view.xml',
