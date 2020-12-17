fe43c3ace7817d49504d2f42c46ec1ff5322e3ee
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-12-17 16:47:31 -0300

    [IMP] clv_person_summary_jcafb (IMP wizard/person_summary_setup)

--- clv_person_summary_jcafb/wizard/person_summary_setup.py
+++ clv_person_summary_jcafb/wizard/person_summary_setup.py
@@ -46,7 +46,6 @@ class PersonSummarySetUp(models.TransientModel):
         default=_default_file_name
     )
 
-    # @api.multi
     def _reopen_form(self):
         self.ensure_one()
         action = {
@@ -59,7 +58,6 @@ class PersonSummarySetUp(models.TransientModel):
         }
         return action
 
-    # @api.multi
     def do_person_summary_setup(self):
         self.ensure_one()
 
@@ -71,7 +69,6 @@ class PersonSummarySetUp(models.TransientModel):
 
         return True
 
-    # @api.multi
     def do_populate_all_persons(self):
         self.ensure_one()
 
